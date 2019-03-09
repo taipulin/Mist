0215e44ae496ad0d24da88bec9757e37520204e8
daniellin <daniellin@DANIELLIN20DC>
2019-03-09 08:46:11 +0800

    Add project files.

---
+++ MVCRockers1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCRockers1.MvcApplication" Language="C#" %>
---
+++ MVCRockers1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCRockers1.Startup))]
+namespace MVCRockers1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
