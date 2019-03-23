d901ea2a129cb68df8a1d04e4213f4bee7ed1182
daniel <daniel.wolf0527@outlook.com>
2019-03-23 09:32:40 +0800

    routes update

--- MVCRockers1/App_Start/RouteConfig.cs
+++ MVCRockers1/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace MVCRockers1
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+            name: "Backstage Pass",
+            url: "backstage/{secret}/{format}",
+            defaults: new { controller = "Home", action = "Backstage", format = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- MVCRockers1/Controllers/HomeController.cs
+++ MVCRockers1/Controllers/HomeController.cs
@@ -26,5 +26,20 @@ namespace MVCRockers1.Controllers
 
             return View();
         }
+
+        public ActionResult Backstage(string secret, string format)
+        {
+            if (secret != "special")
+                return new HttpStatusCodeResult(403);
+
+            if (format == "text")
+                return Content("You Rock!");
+            else if (format == "json")
+                return Json(new { password = "You Rcok!", expires = DateTime.UtcNow.ToShortDateString() },
+                    JsonRequestBehavior.AllowGet);
+            else if (format == "clean")
+                return PartialView();
+            return View();
+        }
     }
 }
